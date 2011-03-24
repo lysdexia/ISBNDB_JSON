ee106a0367382785dfedc755ac22885fc8e37159
Doug <doug@airstream.(none)>
2011-03-24 13:05:55 -0400

    Lunchtime fix! Added logic to flatten a nested tag (i.e. "Price"). Only works for one nested tag! More than one will break. isbndb api shows only single nested per reply, so should be okay. Otherwise will have to bite the bullet and use an XSLT sheet. Ew.

--- isbndb_json.py
+++ isbndb_json.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
-import urllib2, urllib, json
+import urllib2, urllib, json, copy
 import xml.etree.cElementTree as ET
 
 class Query(object):
+    # query isbndb and convert output into book objects
+ 
+    def isbn(self, isbn, results=None):
+        # query by ISBN number
+        query = { 
+            "index1": "isbn",
+            "access_key": access_key,
+            "value1": isbn,
+            }
+        if results:
+            query["results"] = results
+        return self.isbndb(query)
 
     def isbndb(self, query):
+        # returns a list of books in object form
         self.remote_error = None
         args = urllib.urlencode(query)
         request = urllib2.Request(isbndb, args)
@@ -16,59 +29,80 @@ class Query(object):
             self.remote_error = error.msg
             self.code = error.code
             self.url = error.url
-        return response
-
-    
-    def isbn(self, isbn, results=None):
-        query = { 
-            "index1": "isbn",
-            "access_key": access_key,
-            "value1": isbn,
-            }
-        if results:
-            query["results"] = results
-        return self.isbndb(query)
-
-class Book(object):
-
-    # for results w/o nested pricing
-    def flat_results(self, response):
+        return self.flatten_results(response)
+   
+    def flatten_results(self, response):
+        # Flatten all objects into multiple book/author/whatever objects
+        # Will break if more than one nested type. Thus far: no more than 
+        # one nested type. :-)
+        books = []
         if not response:
-            return
+            return books
         try:
             xml_obj = ET.fromstring(response)
         except:
-            return
-        for parent, child in self._iterparent(xml_obj):
-            for item in parent.items():
-                setattr(self, item[0], item[1])
-            setattr(self, child.tag, child.text)
-            for item in child.items():
-                setattr(self, item[0], item[1])
+            return books
 
-    def json(self):
-        _json = {}
-        for i in self.__dict__:
-            _json[i] = self.__dict__[i]
-        return json.dumps(_json)
-        
+        # create our initial book object
+        book = self._book_obj(xml_obj)
+                
+        found_nested = []
+        for tag in book.__dict__:
+            nested = xml_obj.findall(".//%s"%tag)
+            if len(nested) >1:
+                found_nested = nested
+        if not found_nested:
+            return [book]
+
+        for nested in found_nested:
+            books.append(self._book_obj(nested, copy.deepcopy(book)))
+        return books
+
+    def _book_obj(self, xml_obj, book=None):
+        # Iterate over the xml object and place all values into book object
+        # Create a new book if initial book object not provided.
+        if not book:
+            book = Book()
+        for parent, child in self._iterparent(xml_obj):
+            setattr(book, parent.tag, parent.text.strip())
+            for item in parent.items():
+                setattr(book, item[0], item[1].strip())
+            setattr(book, child.tag, child.text)
+            for item in child.items():
+                setattr(book, item[0], item[1].strip())
+        return book
+    
     def _iterparent(self, tree):
+        # the usual pattern
         for parent in tree.getiterator():
             for child in parent:
                 yield parent, child
 
+class Book(object):
+
+    def json(self):
+        # spit out vars in json format
+        _json = {}
+        for i in self.__dict__:
+            _json[i] = self.__dict__[i]
+        return json.dumps(_json)
+       
+# testing. 
 if __name__ == "__main__":
 
     access_key = "XXXXXXXX"
     isbndb = "http://isbndb.com/api/books.xml"
 
-    books = {
+    test_books = {
         "propy": "0596000855",
         }
     
-    for b in books:
-        book = Book()
-        for i in ["details", "texts"]:
+    for b in test_books:
+        for i in ["prices", "details", "texts"]:
             q = Query()
-            book.flat_results(q.isbn(books[b], i))
-            print (book.json())
+            books = q.isbn(test_books[b], i)
+            print ("%s %s"%(i, len(books)))
+            #for book in books:
+            #    if hasattr(book, "price"):
+            #        print (book.json())
+            #        print ("%s %s"%(book.currency_code, book.price))
